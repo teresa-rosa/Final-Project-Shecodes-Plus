c5c9deb58c6601c6331f358f81d93be316e97d8b
teresa-rosa <teresarosa28@gmail.com>
2021-12-27 12:45:10 -0100

    Add units conversion

--- src/app.js
+++ src/app.js
@@ -132,6 +132,19 @@ function handleClick(event) {
     search(userCityName);
 }
 
+let searchBox = document.querySelector("#city-input");
+searchBox.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.querySelector("#search-city-btn").click();
+    }
+});
+
+let searchBoxBtn = document.querySelector("#search-city-btn");
+searchBoxBtn.addEventListener("click", handleClick);
+
+search("Lisbon");
+
 //Forecast
 function formatDay(timestamp) {
     let date = new Date(timestamp * 1000);
@@ -165,6 +178,7 @@ function displayForecast(response) {
                                    >
                                       ${Math.round(forecastDay.temp.max)}º
                                    </span>
+                                   <br/>
                                    <span
                                        class="weather-forecast-temperature-min"
                                    >
@@ -186,15 +200,135 @@ function getForecast(coordinates) {
     axios.get(apiUrl).then(displayForecast);
 }
 
-let searchBox = document.querySelector("#city-input");
-searchBox.addEventListener("keyup", function (event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        document.querySelector("#search-city-btn").click();
+// C to F
+function convertCelsiusToFahrenheit(celsiusTemperature) {
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    return fahrenheitTemperature;
+}
+
+// F to C
+function convertFahrenheitToCelsius(fahrenheitTemperature) {
+    let celsiusTemperature = ((fahrenheitTemperature - 32) * 5) / 9;
+    return celsiusTemperature;
+}
+
+let currentUnit = "C";
+function handleSwitch(event) {
+    if (currentUnit === "C") {
+        // Main temperature F
+        let temperatureElement = document.querySelector("#main-temperature");
+        let celsiusTemperature = temperatureElement.innerHTML.replace("ºC", "");
+        temperatureElement.innerHTML = `${Math.round(
+            convertCelsiusToFahrenheit(celsiusTemperature)
+        )}F`;
+
+        // Max and Min temperature
+        let maxTemperatureElement = document.querySelector("#maxTemperature");
+        let maxCelsiusTemperature = maxTemperatureElement.innerHTML.replace(
+            "º max",
+            ""
+        );
+        maxTemperatureElement.innerHTML = `${Math.round(
+            convertCelsiusToFahrenheit(maxCelsiusTemperature)
+        )}F max`;
+
+        let minTemperatureElement = document.querySelector("#minTemperature");
+        let minCelsiusTemperature = minTemperatureElement.innerHTML.replace(
+            "º min",
+            ""
+        );
+        minTemperatureElement.innerHTML = `${Math.round(
+            convertCelsiusToFahrenheit(minCelsiusTemperature)
+        )}F min`;
+
+        // Max temperature Forecast
+        let maxTemperatureElements = document.querySelectorAll(
+            ".weather-forecast-temperature-max"
+        );
+        maxTemperatureElements.forEach(function (element) {
+            let maxCelsiusTemperature = element.innerHTML
+                .trim()
+                .replace("º", "");
+            element.innerHTML = `${Math.round(
+                convertCelsiusToFahrenheit(maxCelsiusTemperature)
+            )}F`;
+        });
+
+        // Min temperature Forecast
+        let minTemperatureElements = document.querySelectorAll(
+            ".weather-forecast-temperature-min"
+        );
+        minTemperatureElements.forEach(function (element) {
+            let minCelsiusTemperature = element.innerHTML
+                .trim()
+                .replace("º", "");
+            element.innerHTML = `${Math.round(
+                convertCelsiusToFahrenheit(minCelsiusTemperature)
+            )}F`;
+        });
+
+        // Update units
+        currentUnit = "F";
+    } else {
+        // Conversions to Celsius
+        // Main temperature F
+        let temperatureElement = document.querySelector("#main-temperature");
+        let fahrenheitTemperature = temperatureElement.innerHTML.replace(
+            "F",
+            ""
+        );
+        temperatureElement.innerHTML = `${Math.round(
+            convertFahrenheitToCelsius(fahrenheitTemperature)
+        )}ºC`;
+
+        // Max and Min temperature
+        let maxTemperatureElement = document.querySelector("#maxTemperature");
+        let maxfahrenheitTemperature = maxTemperatureElement.innerHTML.replace(
+            "F max",
+            ""
+        );
+        maxTemperatureElement.innerHTML = `${Math.round(
+            convertFahrenheitToCelsius(maxfahrenheitTemperature)
+        )}º max`;
+
+        let minTemperatureElement = document.querySelector("#minTemperature");
+        let minfahrenheitTemperature = minTemperatureElement.innerHTML.replace(
+            "F min",
+            ""
+        );
+        minTemperatureElement.innerHTML = `${Math.round(
+            convertFahrenheitToCelsius(minfahrenheitTemperature)
+        )}º min`;
+
+        // Max temperature Forecast
+        let maxTemperatureElements = document.querySelectorAll(
+            ".weather-forecast-temperature-max"
+        );
+        maxTemperatureElements.forEach(function (element) {
+            let maxFahrenheitTemperature = element.innerHTML
+                .trim()
+                .replace("F", "");
+            element.innerHTML = `${Math.round(
+                convertFahrenheitToCelsius(maxFahrenheitTemperature)
+            )}º`;
+        });
+
+        // Min temperature Forecast
+        let minTemperatureElements = document.querySelectorAll(
+            ".weather-forecast-temperature-min"
+        );
+        minTemperatureElements.forEach(function (element) {
+            let minFahrenheitTemperature = element.innerHTML
+                .trim()
+                .replace("F", "");
+            element.innerHTML = `${Math.round(
+                convertFahrenheitToCelsius(minFahrenheitTemperature)
+            )}º`;
+        });
+        // Update units
+        currentUnit = "C";
     }
-});
+}
 
-let searchBoxBtn = document.querySelector("#search-city-btn");
-searchBoxBtn.addEventListener("click", handleClick);
-
-search("Lisbon");
+let unitsToggle = document.querySelector("#units-toggle");
+unitsToggle.addEventListener("click", handleSwitch);
